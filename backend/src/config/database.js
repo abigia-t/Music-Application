f0a9e33cc932565e3703a9e69d8005ba55796f4f
abigia-t <abig80014@gmail.com>
2025-09-13 12:01:53 +0300

    final commit

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
       useUnifiedTopology: true,
     });
     
-    console.log(`MongoDB Compass Connected: ${conn.connection.host}`);
+    console.log(`MongoDB Atlas Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error('Database connection error:', error.message);
     process.exit(1);
